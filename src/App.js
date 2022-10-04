0984610e3749c9608cdc21e0f28cc57b269975eb
kit0-0 <111427307+kit0-0@users.noreply.github.com>
2022-10-04 15:40:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-//import { Container, Row, Col } from "react-bootstrap";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react'
 import PhoneSignUp from "./components/PhoneSignUp";
 import OtpReact from './components/OtpReact';
-//import SideBar from './components/Sidebar.jsx'
-//import Navigation from "./components/Navigation";
 
-//import "./App.css";
 
 
 function App() {
